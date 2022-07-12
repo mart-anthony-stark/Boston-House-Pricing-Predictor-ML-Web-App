2e41098814c40fd3e0b5f4414e8a7822fba80f49
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-12 21:03:28 +0800

    completed project

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import shap
 import streamlit as st
+import pandas as pd
+import shap
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.ensemble import RandomForestRegressor
 
@@ -11,55 +11,75 @@ This app predicts the **Boston House Price**!
 """)
 st.write('---')
 
-# Load boston house dataset
+# Loads the Boston House Price Dataset
 boston = datasets.load_boston()
 X = pd.DataFrame(boston.data, columns=boston.feature_names)
-y = pd.DataFrame(boston.target, columns=["MEDV"])
+Y = pd.DataFrame(boston.target, columns=["MEDV"])
 
-st.sidebar.header('Input Parameters')
+# Sidebar
+# Header of Specify Input Parameters
+st.sidebar.header('Specify Input Parameters')
 
 def user_input_features():
-  CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
-  ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
-  INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
-  CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
-  NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
-  RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
-  AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
-  DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
-  RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
-  TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
-  PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
-  B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
-  LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
-  data = {'CRIM': CRIM,
-          'ZN': ZN,
-          'INDUS': INDUS,
-          'CHAS': CHAS,
-          'NOX': NOX,
-          'RM': RM,
-          'AGE': AGE,
-          'DIS': DIS,
-          'RAD': RAD,
-          'TAX': TAX,
-          'PTRATIO': PTRATIO,
-          'B': B,
-          'LSTAT': LSTAT}
-  features = pd.DataFrame(data, index=[0])
-  return features
+    CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
+    ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
+    INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
+    CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
+    NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
+    RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
+    AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
+    DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
+    RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
+    TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
+    PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
+    B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
+    LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
+    data = {'CRIM': CRIM,
+            'ZN': ZN,
+            'INDUS': INDUS,
+            'CHAS': CHAS,
+            'NOX': NOX,
+            'RM': RM,
+            'AGE': AGE,
+            'DIS': DIS,
+            'RAD': RAD,
+            'TAX': TAX,
+            'PTRATIO': PTRATIO,
+            'B': B,
+            'LSTAT': LSTAT}
+    features = pd.DataFrame(data, index=[0])
+    return features
 
 df = user_input_features()
+
+# Main Panel
+
 # Print specified input parameters
 st.header('Specified Input parameters')
 st.write(df)
 st.write('---')
 
-# Build model
+# Build Regression Model
 model = RandomForestRegressor()
-model.fit(X, y)
+model.fit(X, Y)
 # Apply Model to Make Prediction
 prediction = model.predict(df)
 
 st.header('Prediction of MEDV')
 st.write(prediction)
 st.write('---')
+
+# Explaining the model's predictions using SHAP values
+# https://github.com/slundberg/shap
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+st.header('Feature Importance')
+plt.title('Feature importance based on SHAP values')
+shap.summary_plot(shap_values, X)
+st.pyplot(bbox_inches='tight')
+st.write('---')
+
+plt.title('Feature importance based on SHAP values (Bar)')
+shap.summary_plot(shap_values, X, plot_type="bar")
+st.pyplot(bbox_inches='tight')
